499be8b22f192e015a5bd43cbdc317f7cc9664c4
Tim <timfx35@gmail.com>
2025-08-19 22:14:33 +0200

    build inv 2

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,46 +1,45 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const phasermsg = () => {
     return {
-        name: 'phasermsg',
+        name: "phasermsg",
         buildStart() {
             process.stdout.write(`Building for production...\n`);
         },
         buildEnd() {
-            const line = "---------------------------------------------------------";
+            const line =
+                "---------------------------------------------------------";
             const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
             process.stdout.write(`${line}\n${msg}\n${line}\n`);
 
             process.stdout.write(`✨ Done ✨\n`);
-        }
-    }
-}
+        },
+    };
+};
 
 export default defineConfig({
-    base: './',
-    plugins: [
-        react(),
-        phasermsg()
-    ],
-    logLevel: 'warning',
+    base: "./",
+    plugins: [react(), phasermsg(), tailwindcss()],
+    logLevel: "warning",
     build: {
         rollupOptions: {
             output: {
                 manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
+                    phaser: ["phaser"],
+                },
+            },
         },
-        minify: 'terser',
+        minify: "terser",
         terserOptions: {
             compress: {
-                passes: 2
+                passes: 2,
             },
             mangle: true,
             format: {
-                comments: false
-            }
-        }
-    }
+                comments: false,
+            },
+        },
+    },
 });
